a5e64f4a0513f15b3421b30c5b23face4e5c3b80
Oscar <o.aguayo02@ufromail.cl>
2022-09-30 15:05:06 -0400

    se sube proyecto

--- src/main/java/main.java
+++ src/main/java/main.java
@@ -2,4 +2,7 @@ public class main {
     public static void main(String[] args) {
         System.out.println("Hola mundo");
     }
+    public int sumar(int num1, int num2){
+        return num1 + num2;
+    }
 }
